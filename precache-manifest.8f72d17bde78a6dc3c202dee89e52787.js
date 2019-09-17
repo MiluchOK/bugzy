00defdd3ebde520f6aee783f180511ca55562be5
Alex Milyukov <amilyukov@apple.com>
2019-09-17 00:02:15 -0700

    Updates

---
+++ precache-manifest.8f72d17bde78a6dc3c202dee89e52787.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f8dd42fdf5ca314237bbb2ddabda70",
+    "url": "/bugzy/index.html"
+  },
+  {
+    "revision": "c6c0967290c25615df5d",
+    "url": "/bugzy/static/css/main.cbc8317d.chunk.css"
+  },
+  {
+    "revision": "168c52ebb856e9cdb8e1",
+    "url": "/bugzy/static/js/2.2b690afb.chunk.js"
+  },
+  {
+    "revision": "c6c0967290c25615df5d",
+    "url": "/bugzy/static/js/main.8900a54b.chunk.js"
+  },
+  {
+    "revision": "087ff417cd9897b0c7dd",
+    "url": "/bugzy/static/js/runtime~main.2607f8d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bugzy/static/media/logo.5d5d9eef.svg"
+  }
+]);
