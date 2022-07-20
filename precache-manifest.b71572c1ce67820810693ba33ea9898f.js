1aa62acfcb04a5ec3b37b7336275c763dd6d6b2e
miluchok <milyukov.alexey@gmail.com>
2022-07-20 14:56:19 +0000

    Updates

---
+++ precache-manifest.b71572c1ce67820810693ba33ea9898f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e31702b1f32f8e2dfbb895cb41f117ee",
+    "url": "/bugzy/index.html"
+  },
+  {
+    "revision": "524ff2b2bb9306537bef",
+    "url": "/bugzy/static/css/main.85641beb.chunk.css"
+  },
+  {
+    "revision": "a067b9066828b5697556",
+    "url": "/bugzy/static/js/2.9a555ac0.chunk.js"
+  },
+  {
+    "revision": "524ff2b2bb9306537bef",
+    "url": "/bugzy/static/js/main.1371d12c.chunk.js"
+  },
+  {
+    "revision": "6d454f3757494af3656a",
+    "url": "/bugzy/static/js/runtime~main.745f94ce.js"
+  }
+]);
